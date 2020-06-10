bb3b4d8b2d473f079e17dc43d968888806b5d5a8
danielliao22 <43585541+danielliao22@users.noreply.github.com>
2020-06-09 22:14:50 -0700

    Toon shader applied with god rays

--- Sword In The Stone/Assets/Toon Shader/CustomLighting.hlsl
+++ Sword In The Stone/Assets/Toon Shader/CustomLighting.hlsl
@@ -31,17 +31,31 @@ void MainLight_half(float3 WorldPos, out half3 Direction, out half3 Color, out h
     DistanceAtten = 1;
     ShadowAtten = 1;
 #else
-#if SHADOWS_SCREEN
-    half4 clipPos = TransformWorldToHClip(WorldPos);
-    half4 shadowCoord = ComputeScreenPos(clipPos);
-#else
-    half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-#endif
-    Light mainLight = GetMainLight(shadowCoord);
-    Direction = mainLight.direction;
-    Color = mainLight.color;
-    DistanceAtten = mainLight.distanceAttenuation;
-    ShadowAtten = mainLight.shadowAttenuation;
+    #if SHADOWS_SCREEN
+        half4 clipPos = TransformWorldToHClip(WorldPos);
+        half4 shadowCoord = ComputeScreenPos(clipPos);
+    #else
+        half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    #endif
+        Light mainLight = GetMainLight(shadowCoord);
+        Direction = mainLight.direction;
+        Color = mainLight.color;
+        DistanceAtten = mainLight.distanceAttenuation;
+        ShadowAtten = mainLight.shadowAttenuation;
+
+    #if !defined(_MAIN_LIGHT_SHADOWS) || defined(_RECEIVE_SHADOWS_OFF)
+        ShadowAtten = 1.0h;
+    #endif
+
+    #if SHADOWS_SCREEN
+        ShadowAtten = SampleScreenSpaceShadowmap(shadowCoord);
+    #else  
+        ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+        half shadowStrength = GetMainLightShadowStrength();
+        ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture,
+        sampler_MainLightShadowmapTexture),
+        shadowSamplingData, shadowStrength, false);
+    #endif
 #endif
 }
 
